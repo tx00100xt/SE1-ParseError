9823c73aad16cc43daf54bbca3925b8378232b7a
tx00100xt <t.x00100x.t@yandex.ru>
2022-05-11 14:14:37 +0300

    Add lost files

---
+++ Sources/build-linux32.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+NCPU=`cat /proc/cpuinfo |grep vendor_id |wc -l`
+let NCPU=$NCPU
+echo "Will build with 'make -j$NCPU' ... please edit this script if incorrect."
+
+set -e
+set -x
+
+rm -rf cmake-build
+
+mkdir $_
+cd $_
+cp -vfr ../EntitiesMP/PlayerWeapons_old.es ../EntitiesMP/PlayerWeapons.es
+
+#cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 ..
+#ninja
+
+# This is the eventual path for amd64.
+#cmake -DCMAKE_BUILD_TYPE=Debug ..
+
+# Right now we force x86, though...
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_FLAGS=-mmmx -DCMAKE_CXX_FLAGS=-mmmx -DUSE_I386_NASM_ASM=TRUE .. $1
+echo "ECC first"
+make ecc
+echo "Then the rest..."
+make -j$NCPU
+
+cp -vfr Debug/*.so ../../x32/SamTSE/Mods/PESE2/Bin
+cp -vfr Debug/libEntitiesMP.so ../../x32/SamTFE/Mods/PEFE2/Bin/libEntities.so
+cp -vfr Debug/libGameMP.so ../../x32/SamTFE/Mods/PEFE2/Bin/libGame.so
+
