7b75bd0ecb7e715a958398b215a63f5ce6a0a0d6
tx00100xt <t.x00100x.t@yandex.ru>
2022-12-26 19:27:01 +0300

    Updated build scripts.

--- Sources/build-linux64xplus.sh
+++ Sources/build-linux64xplus.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 NCPU=`cat /proc/cpuinfo |grep vendor_id |wc -l`
-let NCPU=$NCPU
+
+if [[ $NCPU -eq 0 ]]
+then
+  let NCPU='4'
+else
+  let NCPU=$NCPU
+fi
+
 echo "Will build with 'make -j$NCPU' ... please edit this script if incorrect."
 
 set -e
@@ -17,7 +24,7 @@ cp -vfr ../EntitiesMP/PlayerWeaponsHD.es ../EntitiesMP/PlayerWeapons.es
 #ninja
 
 # This is the eventual path for amd64.
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .. $1
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .. $1 $2
 
 # Right now we force x86, though...
 #cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 ..
